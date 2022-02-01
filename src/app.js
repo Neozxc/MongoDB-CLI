b5b605d4b6dfad7bfe34b2ff9fd1eea8032e77ae
Lapinas <mrlapinas99@gmail.com>
2022-02-01 15:46:39 +0000

    Push movies

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const yargs = require("yargs");
+const { client, connection } = require("./db/connection");
+const { addMovie } = require("./utils");
+
+const app = async (yargsObj) => {
+    try {
+        const collection = await connection();
+    if (yargsObj.add) {
+        await addMovie(collection, {
+            title: yargsObj.title,
+            actor: yargsObj.actor
+        });
+        // add movie to the mongodb database,
+        // needs collection and success message.
+    } else {
+        console.log("Incorrect command.");
+    }
+    client.close();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+app(yargs.argv);
--- src/db/connection.js
+++ src/db/connection.js
@@ -2,3 +2,18 @@ require("dotenv").config();
 const { MongoClient } = require("mongodb");
 
 const client = new MongoClient(process.env.MONGODB);
+
+const connection = async () => {
+    try {
+        await client.connect();
+        const db = client.db("Movies");    
+        return db.collection("Movie");
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+module.exports = {
+    client,
+    connection
+};
